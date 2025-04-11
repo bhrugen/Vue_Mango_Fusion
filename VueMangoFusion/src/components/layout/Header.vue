6b429bef186b6e30cefceebbac52e6323410074c
Bhrugen Patel <bhrugen1990@gmail.com>
2025-04-11 12:12:39 -0500

    Section 20 - Header Part 1

--- VueMangoFusion/src/components/layout/Header.vue
+++ VueMangoFusion/src/components/layout/Header.vue
@@ -1,1 +1,84 @@
-<template>Header</template>
+<template>
+  <nav
+    class="navbar navbar-expand-md rounded-3 mx-auto my-2 w-100 bg-success bg-opacity-10 text-success"
+  >
+    <div class="container-fluid">
+      <router-link class="navbar-brand" :to="{ name: APP_ROUTE_NAMES.HOME }">
+        <img
+          src="@/assets/logo.png"
+          alt="Logo"
+          height="28px"
+          class="d-inline-block align-text-top me-2"
+        />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" :to="{ name: APP_ROUTE_NAMES.HOME }"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              aria-current="page"
+              :to="{ name: APP_ROUTE_NAMES.ORDER_LIST }"
+              >Orders</router-link
+            >
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Admin
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <router-link
+                  class="dropdown-item"
+                  aria-current="page"
+                  :to="{ name: APP_ROUTE_NAMES.MENU_ITEM_LIST }"
+                  >Menu Items</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  class="dropdown-item"
+                  aria-current="page"
+                  :to="{ name: APP_ROUTE_NAMES.MANAGE_ORDER_ADMIN }"
+                  >Order Management</router-link
+                >
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { APP_ROUTE_NAMES } from '@/constants/routeNames'
+</script>
