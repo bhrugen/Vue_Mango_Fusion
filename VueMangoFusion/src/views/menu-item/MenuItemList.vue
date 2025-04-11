d7aad14313d8804b0c12f1899892dbfdb51bbccb
Bhrugen Patel <bhrugen1990@gmail.com>
2025-04-11 13:19:11 -0500

    Section 21 - Fetch Menu Items

--- VueMangoFusion/src/views/menu-item/MenuItemList.vue
+++ VueMangoFusion/src/views/menu-item/MenuItemList.vue
@@ -1,1 +1,22 @@
-<template>Menu Item List</template>
+<template>Menu Item List {{ menuItems }}</template>
+<script setup>
+import menuitemService from '@/services/menuitemService.js'
+import { ref, onMounted } from 'vue'
+
+const menuItems = ref([])
+const loading = ref(false)
+
+const fetchMenuItems = async () => {
+  loading.value = true
+  try {
+    menuItems.value = await menuitemService.getMenuItems()
+    console.log(menuItems.value)
+  } catch (error) {
+    console.log('Error fetch menu items:', error)
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(fetchMenuItems)
+</script>
