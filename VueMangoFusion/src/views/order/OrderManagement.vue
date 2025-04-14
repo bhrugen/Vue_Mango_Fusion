5a9f501c0a59f24bd4ed4db8f3bd4a30372ac98a
Bhrugen Patel <bhrugen1990@gmail.com>
2025-04-14 13:29:33 -0500

    Section 26 - Base for Filter and Sort

--- VueMangoFusion/src/views/order/OrderManagement.vue
+++ VueMangoFusion/src/views/order/OrderManagement.vue
@@ -143,3 +143,46 @@
     <!-- Order Details Modal Component -->
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted, computed } from 'vue'
+import orderService from '@/services/orderService'
+import { APP_ROUTE_NAMES } from '@/constants/routeNames'
+
+const orders = reactive([])
+const loading = ref(false)
+
+//filter and sorting
+const statusFilter = ref('')
+const searchQuery = ref('')
+const sortBy = ref('orderHeaderId')
+const sortDirection = ref('desc')
+
+//pagination
+const itemsPerPage = 5
+const currentPage = ref(1)
+
+const resetFilters = () => {
+  statusFilter.value = ''
+  searchQuery.value = ''
+  sortBy.value = 'orderHeaderId'
+  sortDirection.value = 'desc'
+  currentPage.value = 1
+}
+
+const fetchOrders = async () => {
+  orders.length = 0
+  loading.value = true
+  try {
+    var result = await orderService.getOrders()
+    orders.push(...result)
+    console.log(orders)
+  } catch (error) {
+    console.log('Error fetch orders:', error)
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(fetchOrders)
+</script>
